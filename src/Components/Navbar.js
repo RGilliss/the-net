7accb7b4e77c18b0b121c01b50d831e5ebf7828b
elodiebhs <elodiebouthors@hotmail.com>
2021-04-08 20:07:22 -0700

    change navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { withStyles, makeStyles, fade } from '@material-ui/core/styles';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -8,6 +7,11 @@ import ListItemText from '@material-ui/core/ListItemText';
 import StarsIcon from '@material-ui/icons/Stars';
 import PinDropIcon from '@material-ui/icons/PinDrop';
 import MenuOpenIcon from '@material-ui/icons/MenuOpen';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+
 
 const StyledMenu = withStyles({
   paper: {
@@ -40,6 +44,63 @@ const StyledMenuItem = withStyles((theme) => ({
   },
 }))(MenuItem);
 
+const useStyles = makeStyles((theme) => ({
+
+  title: {
+    flexGrow: 1,
+    color: 'white',
+
+
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputRoot: {
+    color: 'white',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+
+
+  navbar: {
+    display: 'flex',
+    height: '50px',
+    padding: '2rem',
+  }
+}));
+
 export default function CustomizedMenus() {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -51,17 +112,40 @@ export default function CustomizedMenus() {
     setAnchorEl(null);
   };
 
+  const classes = useStyles();
+
   return (
-    <div>
-     
+    <div className={classes.navbar}>
+
       <MenuOpenIcon
-      aria-controls="customized-menu"
-      aria-haspopup="true"
-      variant="contained"
-      color="primary"
-      onClick={handleClick}
-      
+        aria-controls="customized-menu"
+        aria-haspopup="true"
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+
       />
+
+      <Typography variant="h6" className={classes.title}>
+        The Net
+      </Typography>
+
+
+
+
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+      <InputBase
+        placeholder="Search…"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ 'aria-label': 'search' }}
+      />
+
+
 
 
       <StyledMenu
@@ -83,7 +167,7 @@ export default function CustomizedMenus() {
           </ListItemIcon>
           <ListItemText primary="My pins" />
         </StyledMenuItem>
-        
+
       </StyledMenu>
     </div>
   );
