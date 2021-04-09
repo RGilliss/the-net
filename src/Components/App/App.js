edcf47cd9e178a42c32b6625c71e5b7e5c0c2ac9
elodiebhs <elodiebouthors@hotmail.com>
2021-04-08 18:29:54 -0700

    create navbar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./App.css";
+import Navbar from "../Navbar";
 
 export default function App() {
   let position = [49.499998, -125.499998]
   
   return (
+    
+    <div>
+    <Navbar/>
     <MapContainer center={position} zoom={9}>
       <TileLayer
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
@@ -18,5 +22,6 @@ export default function App() {
         </Popup>
       </Marker>
     </MapContainer>
+    </div>
   );
 }
