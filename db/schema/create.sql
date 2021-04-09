908974acae379cf8ec6cc2393e1032ff8a72ff8a
LB <lrbutler@gmail.com>
2021-04-09 14:38:07 -0700

    Add regulation table to schema

--- db/schema/create.sql
+++ db/schema/create.sql
@@ -48,3 +48,16 @@ CREATE TABLE pins (
   species_id INTEGER REFERENCES species (id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
 );
+
+DROP TABLE IF EXISTS regulations CASCADE;
+
+CREATE TABLE regulations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  water_body VARCHAR(255) NOT NULL,
+  class_water BOOLEAN DEFAULT FALSE,
+  tributary BOOLEAN DEFAULT FALSE,
+  stocked BOOLEAN DEFAULT FALSE,
+  accessible BOOLEAN DEFAULT FALSE,
+  date_range VARCHAR(255),
+  regulation TEXT
+);
