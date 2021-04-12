6e299cb8d08028531c29af08b35dff2e0acb39e6
elodiebhs <elodiebouthors@hotmail.com>
2021-04-12 11:14:21 -0700

    add selector to the form for species

--- src/Components/MarkerForm.jsx
+++ src/Components/MarkerForm.jsx
@@ -4,12 +4,14 @@ import Input from '@material-ui/core/Input'
 import InputLabel from '@material-ui/core/InputLabel'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme  } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
 
 
@@ -26,9 +28,25 @@ const useStyles = makeStyles((theme) => ({
   add_picture: {
     margin: theme.spacing(1),
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
 
 }))
 
+//Rating
 const labels = {
   0.5: 'Useless',
   1: 'Useless+',
@@ -42,16 +60,59 @@ const labels = {
   5: 'Excellent+',
 };
 
+
+//Species Menu
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const species = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+function getStyles(name, fishName, theme) {
+  return {
+    fontWeight:
+      fishName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+
 export default function MarkerForm(props) {
   const [value, setValue] = React.useState(2);
   const [hover, setHover] = React.useState(-1);
 
   const classes = useStyles();
+  const theme = useTheme();
+  const [fishName, setFishName] = React.useState([]);
+
+
+  const handleChange = (event) => {
+    setFishName(event.target.value);
+  };
 
   const onSubmit = (evt) => {
     console.log(props.marker);
     props.onClose();
-  }
+  };
 
 
   return (
@@ -70,9 +131,23 @@ export default function MarkerForm(props) {
         defaultValue="2017-05-24"
       />
 
-      <FormControl>
-        <InputLabel htmlFor="Type of fish">Type of fish</InputLabel>
-        <Input id="Type of fish" aria-describedby="my-helper-text" />
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-mutiple-name-label">Species</InputLabel>
+        <Select
+          labelId="demo-mutiple-name-label"
+          id="demo-mutiple-name"
+          multiple
+          value={fishName}
+          onChange={handleChange}
+          input={<Input />}
+          MenuProps={MenuProps}
+        >
+          {species.map((name) => (
+            <MenuItem key={name} value={name} style={getStyles(name, fishName, theme)}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
       </FormControl>
 
       <div className={classes.rating_root}>
