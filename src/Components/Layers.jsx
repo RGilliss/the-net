89e6d9f805db826e0866dd54e419ef8a30ed5c32
elodiebhs <elodiebouthors@hotmail.com>
2021-04-15 17:20:32 -0700

    add comments to code

--- src/Components/Layers.jsx
+++ src/Components/Layers.jsx
@@ -8,15 +8,22 @@ const layerNames = [" My Pins", " All Pins", " My Favourites"]
 
 //Controls the layers of the map interface
 export default function Layers() {
+  //Hook to access DOM elements
   const control = useRef();
+
+  //Set All pins as toggled by default
   const [selected, setSelected]= useState("All Pins");
 
-
   useEffect(() => {
+    //targetting the container
     const container = control.current.getContainer()
+   
     const handleClick = function (event){
+      //target a toggle
       const text = event.target.nextSibling.innerText;
+      //return a bolean true if toggled
       const isLayer = layerNames.includes(text);
+     
 
       if (event.target.checked && isLayer){
           setSelected(selected === text.trim() ? "" : text.trim())
