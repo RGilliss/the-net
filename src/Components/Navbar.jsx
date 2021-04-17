cda2b7f8d7d77db4719395a71008e9ea141a59d5
elodiebhs <elodiebouthors@hotmail.com>
2021-04-16 18:42:59 -0700

    changed font and add avatar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,52 +1,68 @@
 import React from 'react';
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    display: 'flex',
+    flexDirect: 'column'
+
   },
+  navLeft: {
+    flexGrow: 1,
+    display: 'flex',
+  },
+
+
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  navBar:{
+  navBar: {
     backgroundColor: '#cfd8dc',
     margin: 0,
     height: "6em"
   },
-  logo:{
+  logo: {
     marginRight: '10px'
   },
-  typo:{
-    fontFamily: 'Courgette',
-  }
+  typo: {
+    fontFamily: 'Kaushan Script',
+  },
   
 
 }));
 
 export default function DenseAppBar() {
   const classes = useStyles();
- 
+
 
   return (
-   
+
     <div className={classes.root}>
-    
+
       <AppBar position="static" className={classes.navBar} >
         <Toolbar variant="dense">
-        <img src="favicon.ico" alt="logo" className={classes.logo} />
-            
-          <Typography variant="h4" className={classes.typo}>
-            The Net
+
+          <div className={classes.navLeft}>
+            <img src="favicon.ico" alt="logo" className={classes.logo} />
+
+            <Typography variant="h4" className={classes.typo}>
+              The Net
           </Typography>
+          </div>
+
+          <Avatar alt="Avatar Remy Sharp" src="https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166__340.jpg" />
+
         </Toolbar>
       </AppBar>
-    
-      </div>
-      
+
+    </div>
+
   );
 }
