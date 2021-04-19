db35281c41ef6ce410f83cbf31ddd6ac385e870f
LB <lrbutler@gmail.com>
2021-04-19 15:21:08 -0700

    Fix favourites route to pass to New Markers and allow edit and delete

--- src/Components/Layers/Filters/Favourites.jsx
+++ src/Components/Layers/Filters/Favourites.jsx
@@ -3,82 +3,65 @@ import { Popup, Marker } from "react-leaflet";
 import PopupDisplay from "./Markers/PopupDisplay";
 import { useContext } from "react";
 import UserContext from "../../UserContext";
- 
+import NewMarkers from "./Markers/NewMarkers";
 
-import L from 'leaflet';
+import L from "leaflet";
+import { LocalPrintshopSharp } from "@material-ui/icons";
 
- // change pin 
- const iconUrl = require('../../../images/marker-icon-2x-gold.png').default;
+// change pin
+const iconUrl = require("../../../images/marker-icon-2x-gold.png").default;
 
- const iconFavPins = new L.Icon({
-   iconUrl,
-   iconRetinaUrl: iconUrl,
-   iconAnchor: null,
-   popupAnchor: [-3, -76],
-   shadowUrl: null,
-   shadowSize: null,
-   shadowAnchor: null,
-   iconSize: new L.Point(25, 38),
-   className: 'leaflet-marker-icon'
- });
+const iconFavPins = new L.Icon({
+  iconUrl,
+  iconRetinaUrl: iconUrl,
+  iconAnchor: null,
+  popupAnchor: [-3, -76],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(25, 38),
+  className: "leaflet-marker-icon",
+});
 
-export default function Favourites() {
+export default function Favourites(props) {
+  const user = useContext(UserContext);
 
-  const user = useContext(UserContext)
-
-  const [{ data, loading, error }] = useAxios({ url: "/favourites",
-    params: {user_id: user.id }
+  const [{ data, loading, error }] = useAxios({
+    url: "/favourites",
+    params: { user_id: user.id },
   });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
 
-  const returnData = data;
-console.log("user ID", user.id)
-  console.log("Favourites returnData", returnData)
-
-// const [favouritesList, setFavouritesList] = useState([]);
-
-// useEffect(
-
-//   () =>
-//     axios
-//       .get("/favourites", user.id)
-//       .then((res) => {
-//         // console.log(res.data);
-//         setFavouritesList(res.data);
-//       })
-//       .catch((err) => {
-//         console.log(err.response.data);
-//       }),
-//   []
-// );
-
-
+  const favourites = data.reduce((acc, val) => {
+    return {
+      ...acc,
+      [val.uuid]: { ...val, leafletLocation: [val.location.x, val.location.y] },
+    };
+  }, {});
+  console.log("FAVOURITES AFTER REDUCER:", favourites);
+  console.log("FAV PROPS", props);
 
   return (
     <>
-      {returnData.map((pin) => (
-
-        <Marker
-          key={pin.id}
-          position={[pin.location.x, pin.location.y]}
-          icon = {iconFavPins}
-  
-        >
-          <Popup>
-            <PopupDisplay
-              title={pin.title}
-              description={pin.description}
-              date={pin.date}
-              image={pin.image}
-              rating={pin.rating}
-              location={pin.location}
-              species_name={pin.species_name}
-              user_id={pin.user_id}
-            />
-          </Popup>
-        </Marker>
-      ))}
+      <NewMarkers
+        markers={favourites}
+        icon={iconFavPins}
+        title={props.title}
+        description={props.description}
+        date={props.date}
+        image={props.image}
+        rating={props.rating}
+        location={props.location}
+        species_name={props.species_name}
+        user_id={props.user_id}
+        edit={props.edit}
+        setEdit={props.setEdit}
+        modal={props.modal}
+        setModal={props.setModal}
+        editPopup={props.editPopup}
+        setEditPopup={props.setEditPopup}
+      />
     </>
   );
 }
--- src/Components/Layers/Filters/MyPins.jsx
+++ src/Components/Layers/Filters/MyPins.jsx
@@ -1,5 +1,5 @@
 import useAxios from "axios-hooks";
-import axios from 'axios';
+import axios from "axios";
 import { Popup, Marker } from "react-leaflet";
 import PopupDisplay from "./Markers/PopupDisplay";
 import UserContext from "../../UserContext";
@@ -7,48 +7,51 @@ import L from "leaflet";
 import { useContext, useState, useEffect } from "react";
 import NewMarkers from "./Markers/NewMarkers";
 
-  // MyPins Marker Color
-  const iconUrl = require('../../../images/marker-icon-2x-red.png').default;
+// MyPins Marker Color
+const iconUrl = require("../../../images/marker-icon-2x-red.png").default;
 
-  const iconMyPins = new L.Icon({
-    iconUrl,
-    iconRetinaUrl: iconUrl,
-    iconAnchor: null,
-    popupAnchor: [-3, -76],
-    shadowUrl: null,
-    shadowSize: null,
-    shadowAnchor: null,
-    iconSize: new L.Point(25, 38),
-    className: 'leaflet-marker-icon'
-  });
-
+const iconMyPins = new L.Icon({
+  iconUrl,
+  iconRetinaUrl: iconUrl,
+  iconAnchor: null,
+  popupAnchor: [-3, -76],
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+  iconSize: new L.Point(25, 38),
+  className: "leaflet-marker-icon",
+});
 
 export default function MyPins(props) {
   const user = useContext(UserContext);
 
   const [{ data, loading, error }] = useAxios({
     url: "/mypins",
-    params: { user_id: user.id }, 
+    params: { user_id: user.id },
   });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
 
   const pins = data.reduce((acc, val) => {
-    return {...acc, [val.uuid]:{...val, leafletLocation: [val.location.x, val.location.y]}};
+    return {
+      ...acc,
+      [val.uuid]: { ...val, leafletLocation: [val.location.x, val.location.y] },
+    };
   }, {});
-  console.log("PIN AFTER REDUCER:", pins)
-
+  console.log("PIN AFTER REDUCER:", pins);
+  console.log("MUY PINS PROPS", props)
   return (
     <>
-    <NewMarkers 
-    icon={iconMyPins}
-    edit={props.edit}
-    setEdit={props.setEdit}
-    modal={props.modal}
-    setModal={props.setModal}
-    editPopup={props.editPopup}
-    setEditPopup={props.setEditPopup}
-    markers={pins}/>
+      <NewMarkers
+        icon={iconMyPins}
+        edit={props.edit}
+        setEdit={props.setEdit}
+        modal={props.modal}
+        setModal={props.setModal}
+        editPopup={props.editPopup}
+        setEditPopup={props.setEditPopup}
+        markers={pins}
+      />
     </>
   );
 }
