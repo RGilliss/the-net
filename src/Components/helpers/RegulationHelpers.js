869319df2ac21e757466a216256068861e21272e
Rye Gilliss <gilliss.rg@gmail.com>
2021-04-20 14:16:54 -0600

    regulations showing as different shapes and sizes

--- src/Components/helpers/RegulationHelpers.js
+++ src/Components/helpers/RegulationHelpers.js
@@ -1,15 +1,28 @@
-export default function waterSize(size) {
+function waterSize(size) {
   //Sets the size of the regulation circle for each body of water
+  console.log("Lake Size", size)
   switch (size) {
     case 'Large':
-      return 2000;
+      return 4000;
     case 'Medium':
-      return 1000;
+      return 3000;
     case 'Small':
       return 500;
     case 'X-small':
       return 250;
     default:
-      return 750;
+      return 500;
   }
 }
+function regulationColor(color) {
+  switch (color) {
+    case null:
+      return 'blue';
+    case 'YELLOW':
+      return 'yellow';
+    case 'RED':
+      return 'red';
+  }
+}
+
+export { regulationColor, waterSize }
