fb3d0ebd7ff5010e62fa1517537ad254b4d61bf2
elodiebhs <elodiebouthors@hotmail.com>
2021-04-20 18:41:39 -0700

    small change testing

--- cypress/integration/Pins.spec.js
+++ cypress/integration/Pins.spec.js
@@ -5,34 +5,34 @@ describe("Pins", () => {
     cy.visit("http://localhost:3002/");
   });
 
-  // it("should create a new pin", () => {
-  //   //dbl click on the map and a form opens
-  //   cy.get('.leaflet-container').dblclick();
+  it("should create a new pin", () => {
+    //dbl click on the map and a form opens
+    cy.get('.leaflet-container').dblclick();
 
-  //   //Enter a title
-  //   cy.get("[data-test-title=title-input]").type("Alan's spot");
+    //Enter a title
+    cy.get("[data-test-title=title-input]").type("Alan's spot");
 
-  //   //select a date --> click on the icon and select a date
-  //   cy.get('#date').click()
+    //select a date --> click on the icon and select a date
+    cy.get('#date').click()
 
-  //   //Select a species -->click on species and select a species
-  //   cy.get("[data-test-species=select-species]").click();
-  //   cy.contains('Bull Trout').click();
+    //Select a species -->click on species and select a species
+    cy.get("[data-test-species=select-species]").click();
+    cy.contains('Bull Trout').click();
 
-  //   // Rate a spot
-  //   cy.get('.MuiRating-root').click();
+    // Rate a spot
+    cy.get('.MuiRating-root').click();
 
-  //   //Enter a comment
-  //   cy.get("[data-test-text=enter-text]").type("Awesome spot");
+    //Enter a comment
+    cy.get("[data-test-text=enter-text]").type("Awesome spot");
 
-  //   // Add a link for pictures
-  //   cy.get("[data-test-link=enter-link]").type("https://fishingbooker.com/blog/media/hero-lures.jpg");
+    // Add a link for pictures
+    cy.get("[data-test-link=enter-link]").type("https://fishingbooker.com/blog/media/hero-lures.jpg");
 
-  //   //click on Submit to create a pin on the map
-  //   cy.contains("SUBMIT").click();
+    //click on Submit to create a pin on the map
+    cy.contains("SUBMIT").click();
     
 
-  // });
+  });
 
   it("should Edit a pin", () => {
     // click on pin 
